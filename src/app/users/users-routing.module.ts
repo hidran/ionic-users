28e132c95726b4e7b91f2f4398f3ee161e5fbaf7
hidran@gmail.com <hidran@gmail.com>
2020-06-19 02:04:15 +0200

    adds user's data by using ion lists and ion-grid

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsersPage } from './users.page';
-import {UserDetailComponent} from "../user-detail/user-detail.component";
 
 const routes: Routes = [
   {
